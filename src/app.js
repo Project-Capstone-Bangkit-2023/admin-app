4d10976e1a27043b1b32b2197dc5c9276752b8de
David Mahbubi <ulrichdavid0370@gmail.com>
2023-05-22 17:25:15 +0700

    Setup handlebars view engine

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
+const path = require('path')
 const express = require('express')
+const { engine } = require('express-handlebars')
 
 const app = express()
 
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname, '..', 'public')))
+
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.render('pages/dashboard')
 })
 
 module.exports = app
