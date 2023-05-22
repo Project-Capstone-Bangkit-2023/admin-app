114d5fa012ea27f6fc75ef63b69d5aaf515c410f
David Mahbubi <ulrichdavid0370@gmail.com>
2023-05-22 17:27:06 +0700

    Added custom layoutsDir option on handlebars engine

--- src/app.js
+++ src/app.js
@@ -4,13 +4,15 @@ const { engine } = require('express-handlebars')
 
 const app = express()
 
-app.engine('handlebars', engine())
+app.engine('handlebars', engine({
+  layoutsDir: path.join(__dirname, 'views', 'layouts')
+}))
 app.set('view engine', 'handlebars')
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'views/pages'))
 app.use(express.static(path.join(__dirname, '..', 'public')))
 
 app.get('/', (req, res) => {
-  res.render('pages/dashboard')
+  res.render('dashboard')
 })
 
 module.exports = app
