703966e74af5a69ca9213aaa938acb52f27a51af
David Mahbubi <ulrichdavid0370@gmail.com>
2023-05-23 23:06:37 +0700

    Integrating Prisma ORM

---
+++ prisma/migrations/20230523160604_init_user/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE `users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `role` ENUM('user', 'admin') NOT NULL DEFAULT 'user',
+    `location` VARCHAR(191) NULL,
+    `age` INTEGER NULL,
+    `cat_pref` VARCHAR(191) NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NULL,
+
+    UNIQUE INDEX `users_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
