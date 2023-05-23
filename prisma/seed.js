27f0986c4a24b5bc379be871e7e0a6a0196a4147
David Mahbubi <ulrichdavid0370@gmail.com>
2023-05-23 23:17:22 +0700

    Added user seeder

---
+++ prisma/seed.js
@@ -0,0 +1,25 @@
+const { PrismaClient } = require('@prisma/client')
+const bcrypt = require('bcryptjs')
+
+const prisma = new PrismaClient()
+
+async function main() {
+  const admin = await prisma.user.upsert({
+    where: { email: 'admin@gmail.com' },
+    update: {},
+    create: {
+      name: 'Admin',
+      email: 'admin@gmail.com',
+      password: await bcrypt.hash('1234', 10)
+    },
+  })
+}
+
+main().then(async () => {
+  await prisma.$disconnect()
+})
+.catch(async e => {
+  console.error(e)
+  await prisma.$disconnect()
+  process.exit(1)
+})
