7220786cddff2f304d354c64bea8e876c41d5ad8
Dafid Prasetyo <dafidpr@gmail.com>
2023-06-15 13:12:26 +0700

    random query

--- src/controllers/api/tourism.controller.js
+++ src/controllers/api/tourism.controller.js
@@ -65,37 +65,64 @@ exports.getRecommendations = async (req, res) => {
 
   const [tourisms, resultUser, rawTourismsArr] = await Promise.all([dataTourism, dataUser, rawTourisms])
 
-  let newUser = []
+  if (dataUser != null) {
+    let data = [];
+    let newUser = []
 
-  for (let i = 0; i < tourisms.length; i++) {
-    const val = Object.values(resultUser[0])
-    val[2] = (val[2] - 3) / 2
-    newUser.push(val)
+    for (let i = 0; i < tourisms.length; i++) {
+      const val = Object.values(resultUser[0])
+      val[2] = (val[2] - 3) / 2
+      newUser.push(val)
+    }
+
+    const resultTourism = tourisms.map(d => {
+      const val = Object.values(d)
+      val[6] = (val[6] - 3) / 2
+      val[7] = val[6] / 50
+      return val
+    })
+
+    const model = await tf.loadLayersModel(`file://${path.resolve(__dirname, 'models', 'model.json')}`)
+    const inputTf = tf.tensor2d(newUser, [newUser.length, newUser[0].length])
+    const inputTf2 = tf.tensor2d(resultTourism, [resultTourism.length, resultTourism[0].length])
+    const output = model.predict([inputTf, inputTf2])
+    const values = output.dataSync()
+    const arr = Array.from(values)
+    const sortedArr = Array.from(arr.keys()).sort((a, b) => arr[a] - arr[b])
+
+    for (let i = 0; i < 10; i++) {
+      data.push(rawTourismsArr[sortedArr[i]])
+    }
+    res.json({
+      status: 'success',
+      data,
+    })
+  } else {
+    let data = await prisma.tourism.findMany({
+      orderBy: {
+        random: true,
+      },
+    })
+    data = data.map((tourism) => ({
+      id: tourism.id,
+      name: tourism.name,
+      picture: tourism.picture,
+      description: tourism.description,
+      category: tourism.category,
+      city: tourism.city,
+      price: tourism.price,
+      rating: tourism.rating,
+      latitude: tourism.latitude,
+      longitude: tourism.longitude,
+      countRating: tourism.tourism_rating.length,
+      created_at: tourism.created_at,
+      updated_at: tourism.updated_at,
+    }));
+    res.json({
+      status: 'success',
+      data,
+    })
   }
-
-  const resultTourism = tourisms.map(d => {
-    const val = Object.values(d)
-    val[6] = (val[6] - 3) / 2
-    val[7] = val[6] / 50
-    return val
-  })
-
-  const model = await tf.loadLayersModel(`file://${path.resolve(__dirname, 'models', 'model.json')}`)
-  const inputTf = tf.tensor2d(newUser, [newUser.length, newUser[0].length])
-  const inputTf2 = tf.tensor2d(resultTourism, [resultTourism.length, resultTourism[0].length])
-  const output = model.predict([inputTf, inputTf2])
-  const values = output.dataSync()
-  const arr = Array.from(values)
-  const sortedArr = Array.from(arr.keys()).sort((a, b) => arr[a] - arr[b])
-
-  let data = [];
-  for (let i = 0; i < 10; i++) {
-    data.push(rawTourismsArr[sortedArr[i]])
-  }
-  res.json({
-    status: 'success',
-    data,
-  })
 }
 
 exports.getTourisms = async (req, res) => {
